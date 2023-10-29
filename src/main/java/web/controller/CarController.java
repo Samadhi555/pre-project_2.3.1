df8b4365adc4ea969f18c7bd1f444ffc9ad86f22
Oleg Labyr <112154900+Samadhi555@users.noreply.github.com>
2023-10-29 12:33:31 +0300

    Commit_1

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,22 +1,23 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.model.Car;
-import web.service.CarServiceImpl;
-
-import java.util.List;
+import web.service.CarService;
 
 @Controller
 public class CarController {
 
-    @GetMapping(value = "/cars")
-    public String getCars(@RequestParam(defaultValue = "5") Integer count, ModelMap model) {
-        CarServiceImpl carsServiceImpl = new CarServiceImpl();
-        List<Car> carsList = carsServiceImpl.carList(count);
-        model.addAttribute("cars", carsList);
+    @Autowired
+    private CarService carService;
+
+    @GetMapping("/cars")
+    public String getCars(@RequestParam(defaultValue = "5") int count, Model model) {
+        model.addAttribute("cars", carService.carList(count));
         return "cars";
     }
+
+
 }
--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,26 +1,28 @@
 package web.service;
 
+import org.springframework.stereotype.Repository;
 import web.model.Car;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+
+@Repository
 public class CarServiceImpl implements CarService {
 
-    private int SERIES = 100;
+    private int series = 100;
 
     @Override
     public List<Car> carList(int count) {
         List<Car> cars;
         {
             cars = new ArrayList<>();
-            cars.add(new Car("car1", "Black", ++SERIES));
-            cars.add(new Car("car2", "Red", ++SERIES));
-            cars.add(new Car("car3", "Green", ++SERIES));
-            cars.add(new Car("car4", "Blue", ++SERIES));
-            cars.add(new Car("car5", "Orange", ++SERIES));
-
+            cars.add(new Car("car1", "Black", ++series));
+            cars.add(new Car("car2", "Red", ++series));
+            cars.add(new Car("car3", "Green", ++series));
+            cars.add(new Car("car4", "Blue", ++series));
+            cars.add(new Car("car5", "Orange", ++series));
         }
         return cars.stream().limit(count).collect(Collectors.toList());
     }
